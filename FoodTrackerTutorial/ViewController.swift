b765744cdf2c1fbe13a57a480583209cda34b8bc
Sean Midford <seanmidford@outlook.com>
2019-04-29 17:39:43 -0400

    Initial commit.

---
+++ FoodTrackerTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerTutorial
+//
+//  Created by Sean-MacbookPro on 2019-04-29.
+//  Copyright © 2019 midfords. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
