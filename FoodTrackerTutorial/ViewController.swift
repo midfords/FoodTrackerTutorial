e63f3b08f83128a4d4ded2e6e974aeb3e574dd4f
Sean Midford <seanmidford@outlook.com>
2019-06-15 19:19:51 -0400

    Rating control checkpoint.

--- FoodTrackerTutorial/ViewController.swift
+++ FoodTrackerTutorial/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate,
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,10 +72,4 @@ class ViewController: UIViewController, UITextFieldDelegate,
         imagePickerController.delegate = self
         present(imagePickerController, animated: true, completion: nil)
     }
-    
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
 }
-
